dc4bf05d27218c97f73de15e84950ee4874a03af
Rodney Thomas <116589621+rodrthomas@users.noreply.github.com>
2022-10-26 09:23:19 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  static void errorI() {
+    System.out.println("Error Code I");
+  }
+  static void errorII() {
+    System.out.println("Error Code II");
+  }
+  static void errorIII() {
+    System.out.println("Error Code III");
+  }
+  static void errorIV() {
+    System.out.println("Error Code IV");
+  }
+  static void errorV() {
+    System.out.println("Error Code V");
+  }
+
+  public static void main(String[] args) {
+    errorI();
+    errorII();
+    errorIII();
+    errorIV();
+    errorV();
+  }
+}
